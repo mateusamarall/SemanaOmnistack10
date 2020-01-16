0b9c507e272335af45cff55afd603d4faa28d140
mateusAmaral <mateus.amaral018@gmail.com>
2020-01-16 19:27:03 -0300

    mobile

---
+++ mobile/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import Routes from './src/routes';
+export default function App() {
+	return (
+		<>
+			<StatusBar barStyle='light-content' backgroundColor='#7D40E4' />
+			<Routes />
+		</>
+	);
+}
